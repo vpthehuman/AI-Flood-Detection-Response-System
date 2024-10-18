ad03f083f8b67954a4580716669602b2a39cf7a8
Vishnupriya Kannan <155794927+vpthehuman@users.noreply.github.com>
2024-10-18 17:39:38 +0800

    Create data_preprocessing.py

---
+++ flood_detection/data_preprocessing.py
@@ -0,0 +1,41 @@
+import os
+from torchvision import transforms
+from torch.utils.data import Dataset
+from PIL import Image
+
+class FloodNetDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.images = [f for f in os.listdir(root_dir) if f.endswith('.png')]
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, self.images[idx])
+        image = Image.open(img_name).convert('RGB')
+        
+        # Assuming flood images have 'flood' in their filename
+        label = 1 if 'flood' in self.images[idx].lower() else 0
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+def get_data_transforms():
+    return {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
