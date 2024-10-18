6e0f25e31eb79384a57c441d87447a58ffc31e04
Vishnupriya Kannan <155794927+vpthehuman@users.noreply.github.com>
2024-10-18 17:55:49 +0800

    Update model.py

--- flood_detection/model.py
+++ flood_detection/model.py
@@ -1,13 +1,32 @@
-import torch
-import torchvision.models as models
-import torch.nn as nn
-
 class FloodDetectionModel(nn.Module):
-    def __init__(self, num_classes=2):
+    def __init__(self):
         super(FloodDetectionModel, self).__init__()
         self.resnet = models.resnet50(pretrained=True)
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
+        self.resnet.fc = nn.Identity()
+        
+        self.upsample = nn.Sequential(
+            nn.ConvTranspose2d(2048, 1024, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 1, kernel_size=2, stride=2),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-        return self.resnet(x)
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
+
+        x = self.resnet.layer1(x)
+        x = self.resnet.layer2(x)
+        x = self.resnet.layer3(x)
+        x = self.resnet.layer4(x)
+
+        x = self.upsample(x)
+        return x
